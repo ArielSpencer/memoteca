7630457d0f98c38bab344cb2b9624cf29a10fcab
Ariel Spencer <arielspencer.tech@gmail.com>
2024-10-14 23:53:54 -0300

    feat(footer): add footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabecalhoComponent],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'memoteca';
 }
