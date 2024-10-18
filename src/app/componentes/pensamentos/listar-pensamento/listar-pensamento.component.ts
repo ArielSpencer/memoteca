8b07bb792bcbfd855fd72397e28d7a244e270ee8
Ariel Spencer <arielspencer.tech@gmail.com>
2024-10-18 20:00:19 -0300

    feat(component): list pensamento component in listar-pensamento view

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PensamentoComponent } from "../pensamento/pensamento.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-listar-pensamento',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, PensamentoComponent, CommonModule],
   templateUrl: './listar-pensamento.component.html',
   styleUrl: './listar-pensamento.component.css'
 })
 export class ListarPensamentoComponent {
   title = 'listar pensamento';
+
+  listaPensamentos = [
+    {
+      conteudo: 'Je vois la vie en rose!',
+      autoria: 'Edith Piaf',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Tout peut s arranger, quelques rayons du ciel d été.',
+      autoria: 'Edith Piaf',
+      modelo: 'modelo2'
+    }
+  ]
+
 }
