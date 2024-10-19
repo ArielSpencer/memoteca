cd73e7905f47a26ae9e8b2f248140453c796cf7b
Ariel Spencer <arielspencer.tech@gmail.com>
2024-10-18 21:33:36 -0300

    feat(component): implement ngClass to dynamically set width based on content length in pensamento component

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -14,16 +14,26 @@ export class ListarPensamentoComponent {
   title = 'listar pensamento';
 
   listaPensamentos = [
-    // {
-    //   conteudo: 'Je vois la vie en rose!',
-    //   autoria: 'Edith Piaf',
-    //   modelo: 'modelo3'
-    // },
-    // {
-    //   conteudo: 'Tout peut s arranger, quelques rayons du ciel d été.',
-    //   autoria: 'Edith Piaf',
-    //   modelo: 'modelo2'
-    // }
+    {
+      conteudo: 'Je vois la vie en rose!',
+      autoria: 'Edith Piaf',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Tout peut s arranger, quelques rayons du ciel d été.',
+      autoria: 'Edith Piaf',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Don t stop me, don t stop me, don t stop me. Hey, hey, hey. Don t stop me, don t stop me, ooh, ooh, ooh. I like it. Don t stop me, don t stop me. Have a good time, good time. Don t stop me, don t stop me.',
+      autoria: 'Freddie Mercury',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'É pau, é pedra, é o fim do caminho. É um resto de toco, é um pouco sozinho. É um caco de vidro, é a vida, é o Sol. É a noite, é a morte, é o laço, é o anzol. É peroba do campo, é o nó da madeira. Caingá, candeia, é o Matita Pereira. É madeira de vento, tombo da ribanceira. É o mistério profundo, é o queira ou não queira.',
+      autoria: 'Tom Jobim',
+      modelo: 'modelo1'
+    }
   ]
 
 }
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,9 +1,10 @@
+import { NgClass } from '@angular/common';
 import { Component, Input, model } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './pensamento.component.html',
   styleUrl: './pensamento.component.css'
 })
@@ -14,5 +15,14 @@ export class PensamentoComponent {
     conteudo: 'Je vois la vie en rose',
     autoria: 'Edith Piaf',
     modelo: 'modelo3'
+  }
+
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length > 256) {
+      return 'pensamento-g';
+    } else if (this.pensamento.conteudo.length > 170) {
+      return 'pensamento-m';
+    }
+    return 'pensamento-p';
   }
 }
